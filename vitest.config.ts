72093c185e5bb5be14e9d104a144e935eb44f5c9
knipl <knipl.gergely@gmail.com>
2025-10-21 14:27:47 +0200

    init project setup

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+    include: ['**/*.{test,spec}.{ts,tsx}'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'vitest.config.ts',
+        'vitest.setup.ts',
+      ],
+    },
+  },
+});
